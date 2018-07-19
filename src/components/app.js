e80f8cbb4077a1d909fef11bbcbaa93720874fd7
Ligia Dolinta <ligiadolinta@gmail.com>
2018-07-19 20:43:40 +0300

    styled clock with CSS grid and other styles

--- src/components/app.js
+++ src/components/app.js
@@ -5,6 +5,7 @@ import Clock from './clock';
 
 export default class App extends Component {
   render() {
+    
     return (
       <div className="grid">
       <div className="grid__title">Birthday Countdown</div>
--- src/components/clock.js
+++ src/components/clock.js
@@ -5,20 +5,20 @@ class Clock extends Component {
         return (
             <div className="clock">
             <div className="clock__days">
-                <label className="clock__title">DAYS</label>
-                <label className="clock__amount">20</label>
+                <label className="clock__title clock__box">DAYS</label>
+                <label className="clock__amount">300</label>
             </div>
             <div className="clock__hours">
-                <label className="clock__title">HOURS</label>
-                <label className="clock__amount">20</label>
+                <label className="clock__title clock__box">HRS</label>
+                <label className="clock__amount">16</label>
             </div>
             <div className="clock__minutes">
-                <label className="clock__title">MINUTES</label>
-                <label className="clock__amount">20</label>
+                <label className="clock__title clock__box">MINS</label>
+                <label className="clock__amount">42</label>
             </div>
             <div className="clock__seconds">
-                <label className="clock__title">SECONDS</label>
-                <label className="clock__amount">20</label>
+                <label className="clock__title clock__box">SEC</label>
+                <label className="clock__amount">22</label>
             </div>
             </div>
         )
